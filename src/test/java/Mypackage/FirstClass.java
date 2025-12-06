6b8b73de8697ff98fee810e2df0cc167dbc2262f
Durga Mahesh <durgamaheshvanum123@ggmail.com>
2025-12-06 18:01:13 +0530

    initial Commit

---
+++ src/test/java/Mypackage/FirstClass.java
@@ -0,0 +1,29 @@
+package Mypackage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstClass {
+    WebDriver driver;
+    @BeforeMethod
+    public void startBrowser(){
+        WebDriverManager.chromedriver().setup();
+         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+    }
+    @Test
+    public void firtstestcase(){
+        System.out.println("First test case passed");
+    }
+
+    @AfterMethod
+    public void close(){
+        driver.close();
+    }
+
+}
